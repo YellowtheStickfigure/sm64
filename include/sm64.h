7af7e64cf3be9a48a5f3811e17317cc092db6d0c
YellowtheStickfigure <158374766+YellowtheStickfigure@users.noreply.github.com>
2024-02-11 08:06:23 -0500

    Update sm64.h

--- include/sm64.h
+++ include/sm64.h
@@ -24,19 +24,19 @@
 // Pointer casting is technically UB, and avoiding it gets rid of endian issues
 // as well as a nice side effect.
 #ifdef AVOID_UB
-#define GET_HIGH_U16_OF_32(var) ((u16)((var) >> 16))
-#define GET_HIGH_S16_OF_32(var) ((s16)((var) >> 16))
-#define GET_LOW_U16_OF_32(var) ((u16)((var) & 0xFFFF))
-#define GET_LOW_S16_OF_32(var) ((s16)((var) & 0xFFFF))
-#define SET_HIGH_U16_OF_32(var, x) ((var) = ((var) & 0xFFFF) | ((x) << 16))
-#define SET_HIGH_S16_OF_32(var, x) ((var) = ((var) & 0xFFFF) | ((x) << 16))
+#define GET_HIGH_U16_OF_64(var) ((u16)((var) >> 16))
+#define GET_HIGH_S16_OF_64(var) ((s16)((var) >> 16))
+#define GET_LOW_U16_OF_64(var) ((u16)((var) & 0xFFFF))
+#define GET_LOW_S16_OF_64(var) ((s16)((var) & 0xFFFF))
+#define SET_HIGH_U16_OF_64(var, x) ((var) = ((var) & 0xFFFF) | ((x) << 16))
+#define SET_HIGH_S16_OF_64(var, x) ((var) = ((var) & 0xFFFF) | ((x) << 16))
 #else
-#define GET_HIGH_U16_OF_32(var) (((u16 *)&(var))[0])
+#define GET_HIGH_U16_OF_64(var) (((u16 *)&(var))[0])
 #define GET_HIGH_S16_OF_32(var) (((s16 *)&(var))[0])
-#define GET_LOW_U16_OF_32(var) (((u16 *)&(var))[1])
-#define GET_LOW_S16_OF_32(var) (((s16 *)&(var))[1])
-#define SET_HIGH_U16_OF_32(var, x) ((((u16 *)&(var))[0]) = (x))
-#define SET_HIGH_S16_OF_32(var, x) ((((s16 *)&(var))[0]) = (x))
+#define GET_LOW_U16_OF_64(var) (((u16 *)&(var))[1])
+#define GET_LOW_S16_OF_64(var) (((s16 *)&(var))[1])
+#define SET_HIGH_U16_OF_64(var, x) ((((u16 *)&(var))[0]) = (x))
+#define SET_HIGH_S16_OF_64(var, x) ((((s16 *)&(var))[0]) = (x))
 #endif
 
 // Layers

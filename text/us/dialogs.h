a9fd6c2bafb4231189c79dcc10609837e45a2eee
YellowtheStickfigure <158374766+YellowtheStickfigure@users.noreply.github.com>
2024-11-06 10:30:36 -0500

    Update dialogs.h

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -47,7 +47,7 @@ always finding ways to\n\
 attack.\n\
 This meadow has become\n\
 a battlefield ever since\n\
-the Big Bob-omb got his\n\
+the King Bob-omb got his\n\
 paws on the Power Star.\n\
 Can you recover the Star\n\
 for us? Cross the bridge\n\
@@ -66,7 +66,7 @@ Cross the two\n\
 bridges ahead, then\n\
 watch for falling\n\
 water bombs.\n\
-The Big Bob-omb at the\n\
+The King Bob-omb at the\n\
 top of the mountain is\n\
 very powerful--don't let\n\
 him grab you!\n\
@@ -76,7 +76,7 @@ You can talk to us\n\
 whenever you'd like to!"))
 
 DEFINE_DIALOG(DIALOG_003, 1, 5, 95, 200, _("\
-Thank you, Mario! The Big\n\
+Thank you, Mario! The King\n\
 Bob-omb is nothing but a\n\
 big dud now! But the\n\
 battle for the castle has\n\
@@ -104,7 +104,7 @@ you to use. Bon Voyage!"))
 
 DEFINE_DIALOG(DIALOG_005, 1, 3, 30, 200, _("\
 Hey, Mario! Is it true\n\
-that you beat the Big\n\
+that you beat the King\n\
 Bob-omb? Cool!\n\
 You must be strong. And\n\
 pretty fast. So, how fast\n\
@@ -114,7 +114,7 @@ Koopa the Quick? I don't\n\
 think so. Just try me.\n\
 How about a race to the\n\
 mountaintop, where the\n\
-Big Bob-omb was?\n\
+King Bob-omb was?\n\
 Whaddya say? When I say\n\
 『Go,』 let the race begin!\n\
 \n\
@@ -239,9 +239,9 @@ ride wherever you want to\n\
 go! Shred those enemies!"))
 
 DEFINE_DIALOG(DIALOG_017, 1, 4, 30, 200, _("\
-I'm the Big Bob-omb, lord\n\
+I'm King Bob-omb, lord\n\
 of all blasting matter,\n\
-king of ka-booms the\n\
+King of the ka-booms the\n\
 world over!\n\
 How dare you scale my\n\
 mountain? By what right\n\
@@ -489,11 +489,11 @@ opens."))
 DEFINE_DIALOG(DIALOG_039, 1, 4, 30, 200, _("\
 No visitors allowed,\n\
 by decree of\n\
-the Big Bob-omb\n\
+the King Bob-omb\n\
 \n\
 I shall never surrender my\n\
 Stars, for they hold the\n\
-power of the castle in\n\
+power of the Castle in\n\
 their glow.\n\
 They were a gift from\n\
 Bowser, the Koopa King\n\
@@ -506,7 +506,7 @@ perhaps one hint:\n\
 Heed the Star names at\n\
 the beginning of the\n\
 course.\n\
-//--The Big Bob-omb"))
+//--The King Bob-omb"))
 
 DEFINE_DIALOG(DIALOG_040, 1, 3, 30, 200, _("\
 Warning!\n\
